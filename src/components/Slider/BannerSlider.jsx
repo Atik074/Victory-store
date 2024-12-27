c7021bb84e4b009263f4e74ae8a2645f4bd0f55b
atik074 <atiksmats@gmail.com>
2024-12-28 00:05:59 +0530

    error solve in custom button swiper btn

--- src/components/Slider/BannerSlider.jsx
+++ src/components/Slider/BannerSlider.jsx
@@ -1,15 +1,19 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import { Navigation,Autoplay } from 'swiper/modules';
 
 const BannerSlider = () => {
     return (
         <Swiper 
         navigation={true}
         spaceBetween={20}
-         modules={[Navigation]} className="container   w-full h-full">
-         <SwiperSlide className='flex items-center content-center bg-[#fff] text-[18px] text-center'>
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+         modules={[Navigation ,Autoplay]} className="container   w-full h-full">
+         <SwiperSlide  className='flex items-center content-center bg-[#fff] text-[18px] text-center'>
                 <div className='overflow-hidden'>
                 <img className='w-full  h-[430px] rounded-md block object-cover' src="https://shorturl.at/gZOaC" alt="" />
                 </div>
--- src/components/Slider/ContentSlider.jsx
+++ src/components/Slider/ContentSlider.jsx
@@ -1,72 +1,80 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation} from "swiper/modules";
+import "./style.css"
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Navigation } from 'swiper/modules';
+
+const contentItems = [
+  {
+    id: "1",
+    img: "http://surl.li/qhuvxh",
+    title: "Smart Tablet",
+  },
+  {
+    id: "2",
+    img: "http://surl.li/kydats",
+    title: "Creape T-Shirt",
+  },
+  {
+    id: "3",
+    img: "http://surl.li/wapskd",
+    title: "Lather Watch",
+  },
+  {
+    id: "4",
+    img: "http://surl.li/xuoble",
+    title: "Rolling Diamond",
+  },
+  {
+    id: "5",
+    img: "http://surl.li/hcmqaq",
+    title: "Wooden Chairs",
+  },
+  {
+    id: "6",
+    img: "http://surl.li/hyohot",
+    title: "Sneakers Shoes",
+  },
+  {
+    id: "7",
+    img: "http://surl.li/bkxlrj",
+    title: "Purse",
+  },
+  {
+    id: "8",
+    img: "http://surl.li/roxenu",
+    title: "Xbox Controller",
+  }
+];
 
 const ContentSlider = () => {
-    return (
-      <Swiper
+  
+
+  return (
+   <div >
+     <Swiper
       slidesPerView={6}
-      spaceBetween={30}
-      modules={[Navigation]} 
+      spaceBetween={2}
+      
+      navigation={true}
+      modules={[Navigation]}
+     
       className="mySwiper container my-10"
     >
-      <SwiperSlide>
-         <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-           <img className='rounded-md relative w-full h-full ' src="http://surl.li/qhuvxh" alt="" />
-          <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-       
+      {contentItems.map((item) => (
+        <SwiperSlide ar key={item.id}>
+          <div className="card-body flex flex-col justify-center items-center bg-white w-[190px] h-[180px] border border-[#dcdde1] rounded-[8px] overflow-hidden">
+            <img className="rounded-md relative" src={`${item.img}`} alt="" />
+            <h3 className="absolute text-[18px] bottom-4 font-medium">
+              {item.title}
+            </h3>
+          </div>
         </SwiperSlide>
-
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/kydats" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-        
-        </SwiperSlide>
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/wapskd" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-         </SwiperSlide>
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/xuoble" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-         </SwiperSlide>
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/hcmqaq" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-        </SwiperSlide>
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/hyohot" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-        </SwiperSlide>
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/bkxlrj" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-        </SwiperSlide>
-      <SwiperSlide>
-      <div className='flex flex-col justify-center items-center bg-white w-[172px] h-[175px] border border-[#dcdde1] rounded-md overflow-hidden' >
-      <img className='rounded-md relative'  src="http://surl.li/roxenu" alt="" />
-      <h3 className='absolute text-[18px] bottom-4 font-medium'>Smart Tablet</h3>
-         </div>
-        </SwiperSlide>
-   
-     
+      ))}
     </Swiper>
-    );
+   </div>
+  );
 };
 
 export default ContentSlider;
