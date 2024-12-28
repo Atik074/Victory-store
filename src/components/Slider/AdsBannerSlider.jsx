869c514e197d7b12e0e01c3493743b7b1e95b413
atik074 <atiksmats@gmail.com>
2024-12-29 00:01:39 +0530

    adds bannerslider error ,something went wrong

---
+++ src/components/Slider/AdsBannerSlider.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+const AdsBannerSlider = ({title,img,price,buttonText}) => {
+    return (
+        <div className=" p-4 relative overflow-hidden ">
+            <img className=" rounded-md hover:scale-110" src={img} alt="picture" />
+
+            <div className=" absolute  w-[180px] top-14 right-6">
+                <h3 className="text-[25px]  font-medium">{title}</h3>
+                <p className="text-[19px] my-1 text-red-500 font-bold">{price}</p>
+                <button className="font-medium hover:underline">{buttonText}</button>
+            </div>
+          
+            
+        </div>
+    );
+};
+
+export default AdsBannerSlider;
--- src/pages/banner/Banner.jsx
+++ src/pages/banner/Banner.jsx
@@ -1,3 +1,4 @@
+import AdsBannerSlider from "../../components/Slider/AdsBannerSlider";
 import BannerSlider from "../../components/Slider/BannerSlider";
 import ContentSlider from "../../components/Slider/ContentSlider";
 
@@ -12,7 +13,7 @@ const  Banner =()=> {
      <ContentSlider/>
      </div>
       {/* free shipping */}
-     <div className="container overflow-hidden flex items-center justify-between border-2 border-red-500  h-24  rounded-md  p-6 my-24">
+     <section className="container overflow-hidden flex items-center justify-between border-2 border-red-500  h-24  rounded-md  p-5 my-24">
         <div className="flex items-center space-x-2 ">
           <img className="h-[100px] scale-x-[-1]" src="https://i.pinimg.com/474x/0f/f8/ce/0ff8ce20452f671134de5af16dc7cbb1.jpg" alt="car" />
           <h2 className="text-3xl font-bold uppercase">Free Shipping </h2>
@@ -21,8 +22,28 @@ const  Banner =()=> {
         <p className="text-xl">Free Delivery Now On Your First Order and over $200</p>
         <p className="border h-10 border-r-[1px] border-[#dfe4ea]"></p>
         <h2 className="text-3xl font-bold uppercase">- Only $200*</h2>
-     </div>
+     </section>
 
+        <section  className="flex p-5">
+               <AdsBannerSlider 
+               title="S22 Samsung Smartphone"
+               img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/cms/cms-banner-1.jpg"
+               price="$250.00" 
+               buttonText="SHOP NOW"
+               />
+               <AdsBannerSlider 
+               title="Armchair Mad By shopstic"
+               img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/cms/cms-banner-2.jpg"
+               price="$190.00" 
+               buttonText="SHOP NOW"
+               />
+               <AdsBannerSlider 
+               title="Noise Wireless Headphones"
+               img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/cms/cms-banner-3.jpg"
+               price="$129.00" 
+               buttonText="SHOP NOW"
+               />
+        </section>
 
     </div>
    
